d1e0d59488c2b977f7f1b8ad3be7adafa7849200
Michael Chen <michael.chen@opendoor.com>
2019-11-30 20:43:54 -0800

    rename so reddit does not get mad

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default class App extends BaseApp {
     return (
       <>
         <Head>
-          <title>reddit upvotes</title>
+          <title>introspect for reddit</title>
         </Head>
         <Component {...pageProps} />
       </>
